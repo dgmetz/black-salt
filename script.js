0cd067f60cdf99a9d853f4b1ddf0743e3a69ff46
drewgm <drewgmetz@gmail.com>
2024-07-23 00:54:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+let lastClickedPoint = null;
+
 document.querySelectorAll('.point').forEach(point => {
     point.addEventListener('click', function() {
+        // Remove the highlight from the last clicked point
+        if (lastClickedPoint) {
+            lastClickedPoint.classList.remove('highlighted');
+        }
+
+        // Highlight the clicked point
+        this.classList.add('highlighted');
+        
+        // Update the info text
         const infoText = this.getAttribute('data-info');
         document.getElementById('info').innerText = infoText;
+
+        // Store the current clicked point
+        lastClickedPoint = this;
     });
 });
